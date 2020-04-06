65058030f0b51e71f35e14f9513cce796c1fe0f8
isabella g henriques <isabellagh88@gmail.com>
2020-04-06 00:40:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,5 @@
 
+<<<<<<< HEAD
 def introduction (name)
   puts "Hi, my name is #{name}."
 end
@@ -6,10 +7,15 @@ end
 introduction("josh")
 
 
+=======
+>>>>>>> 78371d8c97967f5f52317c3fdf07af1260a70c7c
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction_with_language("Isabella", "Ruby")
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 78371d8c97967f5f52317c3fdf07af1260a70c7c
